cfb7b5045d05e713d38c6f8c3bba07e0d0bf673a
ducsiukap <DucPV.contact@gmail.com>
2025-07-19 09:01:03 +0700

    1st commit --- Hello Java

---
+++ JAVA001_HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JAVA001_HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("--------------------");
+        System.out.println("Hi, " + name + "!");
+
+        System.out.println("#vduczz");
+        sc.close();
+    }
+}
